2f54d0a02d6ea440819b8416d84a5fb8b2c61269
Michael Yuan <michael@secondstate.io>
2023-01-13 03:36:46 +0000

    Fix problems
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- move/src/main.rs
+++ move/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-  let hello : String = String::from("Hello ");
+  let hello : String = String::from("Hello");
   take(hello); // From take(): Hello WasmEdge!
   // The following will fail since hello is already taken by take() and no longer available here
-  println!("From main(): {}", hello);
+  // println!("From main(): {}", hello);
 
-  let hello : String = String::from("Hello ");
+  let hello : String = String::from("Hello");
   take(hello.clone()); // From take(): Hello WasmEdge!
   println!("From main(): {}", hello); // From main(): Hello
 
-  let hello : String = String::from("Hello ");
+  let hello : String = String::from("Hello");
   borrow(&hello); // From borrow(): Hello WasmEdge!
   println!("From main(): {}", hello); // From main(): Hello
 }
